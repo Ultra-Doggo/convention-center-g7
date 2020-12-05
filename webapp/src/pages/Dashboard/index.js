76910f90e2b26fc14069f5f4ccbd647a17fb0c5e
Bruno Ebihara <bruno.ebihara@gmail.com>
2020-12-05 12:22:42 -0500

    missing modules

--- webapp/src/pages/Dashboard/index.js
+++ webapp/src/pages/Dashboard/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../webapp.css';
 import {withRouter} from "react-router-dom"
-import {register} from "../../services/Validator";
 
 class Dashboard extends React.Component {
 
