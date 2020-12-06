df1cb640ce8d07b157f2cc6872e8a932ac1d6691
Edward Evangelista <ted11697@gmail.com>
2020-12-06 13:43:58 -0500

    added and styled placeholder table for Find Events page

--- webapp/src/pages/FindEvents/index.js
+++ webapp/src/pages/FindEvents/index.js
@@ -7,6 +7,16 @@ class FindEvents extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleBackToDashboard = this.handleBackToDashboard.bind(this);
+
+		this.state =  {
+			events: [
+				// event name, description, location, date & time, host email, website
+				{ event: 'placeholder ONE', location: 'a place', date: 'a' , website: 'google.com'},
+				{ event: 'placeholder TWO', location: 'that place', date: 'a' , website: 'youtube.com'},
+				{ event: 'placeholder THREE', location: 'this place', date: 'a' , website: 'gmail.com'},
+				{ event: 'placeholder FOUR', location: 'another place', date: 'a' , website: 'site.com'},
+			]
+		}
 	}
 
 	handleBackToDashboard(event) {
@@ -15,12 +25,39 @@ class FindEvents extends React.Component {
         this.props.history.push("/dashboard");
 	}
 
+	renderTableData() {
+		return this.state.events.map((eventInfo, index) => {
+		   const { event, about, location, date, website, email } = eventInfo //destructuring
+		   return (
+			  <tr key={event}>
+				 <td>{event}</td>
+				 <td>{location}</td>
+				 <td>{date}</td>
+				 <td>{website}</td>
+			  </tr>
+		   )
+		})
+	}
+
+	renderTableHeader() {
+		let header = Object.keys(this.state.events[0])
+		return header.map((key, index) => {
+		   return <th key={index}>{key.toUpperCase()}</th>
+		})
+	 }
+
 	render() {
 		return (
-      <div className="container" id="container">
-        <h1>Find Events</h1>
-		<button onClick={this.handleBackToDashboard}> Back to Dashboard </button>
-      </div>
+      		<div className="container" id="container">
+        		<h1>Find Events</h1>
+				<table id='events'>
+               		<tbody>
+						<tr>{this.renderTableHeader()}</tr>
+                  		{this.renderTableData()}
+               		</tbody>
+            	</table>
+				<button id="back-to-dash" onClick={this.handleBackToDashboard}> Back to Dashboard </button>
+      		</div>
 		);
 	}
 }
